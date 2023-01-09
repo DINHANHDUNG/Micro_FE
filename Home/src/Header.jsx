fdba2e663601df1ee52160a47c895b05243a316e
DINH ANH DUNG <dungda@OMINEXT.LOCAL>
2023-01-09 16:32:56 +0700

    styled-components

--- Home/src/Header.jsx
+++ Home/src/Header.jsx
@@ -1,24 +1,40 @@
-import React from "react";
-
+import React, { useState } from "react";
+import styled from 'styled-components';
 function Header(props) {
+  const [show, setShow] = useState(true)
   const clickHeader = (value) =>{
     console.log("asdasdasd",value);
     console.log('props', props);
     props?.CallBack(value)
   }
+  const actionShow = () => {
+    if (window.innerWidth < 1000) {
+      setShow(!show)
+    }
+    
+  }
+  //Không ảnh hưởng khi style trong CssGlobal
+  const Header = styled.h1`
+    margin-top: 0;
+    margin-bottom: 0;
+    background-color: revert;
+  `;
   return (
-    <div className="header">
-      <a className="logo" onClick={()=>clickHeader('')}>
+    <Header className="header">
+      <a className="logo" onClick={()=>{
+        clickHeader('')
+        actionShow()
+      }}>
         HOMEPAGE
       </a>
-      <div className="header-right">
+      {show && <div className="header-right">
         <a className="active" onClick={()=>clickHeader('')}>
           Home
         </a>
         <a onClick={()=>clickHeader('contact')}>Contact</a>
         <a onClick={()=>clickHeader('about')}>About</a>
-      </div>
-    </div>
+      </div>}
+    </Header>
   );
 }
 
